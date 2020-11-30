a06ce31b2f7060f9350859ea81d2c70eb9b03266
Raj Karan Swain <54386422+rajkaranswain@users.noreply.github.com>
2020-11-30 20:30:25 +0530

    Add files via upload

---
+++ problems/assignment1/q3.java
@@ -0,0 +1,11 @@
+import java.util.Properties;
+
+public class q3 {
+    public static void main(String[] args) {
+		
+		Properties props = System.getProperties();
+		props.list(System.out);
+
+	}
+    
+}
