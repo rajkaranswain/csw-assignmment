fa93ef2bb788b3332b927c66dd6e73574bceab1a
Raj Karan Swain <54386422+rajkaranswain@users.noreply.github.com>
2020-11-30 19:49:20 +0530

    Add files via upload
    q1 done

---
+++ problems/assignment1/q1.java
@@ -0,0 +1,11 @@
+public class q1 {
+
+	public static void main(String[] args) {
+
+        String fSeparator = System.getProperty("file.separator");
+        
+		System.out.println("File separator is "+fSeparator);
+
+	}
+
+}
